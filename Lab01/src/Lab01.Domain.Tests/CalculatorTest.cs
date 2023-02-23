93752e4cf9adf534b6005685d71d31cc470c2801
Khaled Zra <halo_434000@hotmail.com>
2023-02-23 15:57:20 +0100

    Added new class to test

--- Lab01/src/Lab01.Domain.Tests/CalculatorTest.cs
+++ Lab01/src/Lab01.Domain.Tests/CalculatorTest.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
 using FluentAssertions;
 
 namespace Lab01.Domain.Tests;
@@ -34,6 +36,41 @@ public class CalculatorTest
 
         // Assert
         result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> GetNumbers()
+    {
+        yield return new object[] {5, 2, 7};
+        yield return new object[] {10, 10, 20};
+        yield return new object[] {15, 15, 30};
+    }
+
+    [Theory]
+    [MemberData(nameof(GetNumbers))]
+    public void Add_two_numbers_member_data(int a, int b, int expectedResult)
+    {
+        // Arrange
+        var sut = new Calculator();
+        
+        // Act
+        int result = sut.Add(a, b);
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+    
+    [Theory]
+    [ClassData(typeof(NumberGenerator))]
+    public void Add_two_numbers_class_data(int a, int b, int expectedResult)
+    {
+        // Arrange
+        var sut = new Calculator();
+        
+        // Act
+        int result = sut.Add(a, b);
+
+        // Assert
+        result.Should().Be(expectedResult);
     }
 }
 
---
+++ Lab01/src/Lab01.Domain.Tests/NumberGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab01.Domain.Tests;
+
+public class NumberGenerator : IEnumerable<object[]>
+{
+    private readonly List<object[]> _data = new List<object[]>();
+
+    public NumberGenerator()
+    {
+        _data.Add(new object[] {5, 2, 7});
+        _data.Add(new object[] {5, 5, 10});
+        _data.Add(new object[] {10, 10, 20});
+    }
+    
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        return _data.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
