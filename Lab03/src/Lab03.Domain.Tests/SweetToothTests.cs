7d827981f70ea6f4cef2a63f881844f3a9059c83
Khaled Zra <halo_434000@hotmail.com>
2023-03-02 15:51:47 +0100

    Added mocking to lab3

---
+++ Lab03/src/Lab03.Domain.Tests/SweetToothTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+
+namespace Lab03.Domain.Tests
+{
+    using FakeItEasy; // Parameterized Input
+    using Xunit; // any test framework will do
+
+    public class SweetToothTests
+    {
+        [Fact]
+        public void BuyTastiestCandy_should_buy_top_selling_candy_from_shop()
+        {
+            // make some fakes for the test
+            var lollipop = A.Fake<ICandy>();
+            var shop = A.Fake<ICandyShop>();
+
+            // set up a call to return a value
+            A.CallTo(() => shop.GetTopSellingCandy()).Returns(lollipop);
+            A.CallTo(() => shop.Address).Returns("123 Fake Street");
+
+            // use the fake as an actual instance of the faked type
+            var developer = new SweetTooth();
+            developer.BuyTastiestCandy(shop);
+
+            // asserting uses the exact same syntax as when configuring calls—
+            // no need to learn another syntax
+            A.CallTo(() => shop.BuyCandy(lollipop)).MustHaveHappened();
+            shop.Address.Should().Be("123 Fake Street");
+        }
+
+        public class SweetTooth
+        {
+            public void BuyTastiestCandy(ICandyShop shop)
+            {
+                shop.BuyCandy(shop.GetTopSellingCandy());
+            }
+        }
+
+        public interface ICandyShop
+        {
+            public ICandy GetTopSellingCandy();
+            public void BuyCandy(ICandy candy);
+            string Address { get; set; }
+        }
+
+        public interface ICandy
+        {
+        }
+    }
+}
